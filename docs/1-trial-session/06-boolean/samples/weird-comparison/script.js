20ca54e87a6c38fa695da6a4ea2723e05546239b
chelproc <chelproc@outlook.com>
2022-04-16 01:37:13 +0000

    fix sample

--- docs/1-trial-session/06-boolean/samples/weird-comparison/script.js
+++ docs/1-trial-session/06-boolean/samples/weird-comparison/script.js
@@ -1,3 +1,5 @@
-let price = 100;
-price = price / 2;
-document.write(price);
+let takaoHeight = 599;
+let everestHeight = 8849;
+let fujiHeight = 3776;
+document.write(takaoHeight < everestHeight < fujiHeight);
+document.write(takaoHeight = everestHeight);
