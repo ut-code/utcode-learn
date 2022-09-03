f5c40a3c18b045424a83efa09e9289ec2cb7be99
kazukinumazato <eigobaka@icloud.com>
2022-09-03 20:05:41 +0900

    掲示板の解答を追加

---
+++ docs/3-web-servers/07-get-post/_samples/bulletin-board/server.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+const messages = [];
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  const template = fs.readFileSync("template.ejs", "utf-8");
+  const html = ejs.render(template, { messages: messages });
+  response.send(html);
+});
+
+app.post("/send", (request, response) => {
+  messages.push(request.body.message);
+  response.send("送信しました")
+});
+
+app.listen(3000);
