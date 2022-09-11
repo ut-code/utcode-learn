3d36267b0b3e981d917bd06904573c1a6fd6d90c
chvmvd <chvmvd@users.noreply.github.com>
2022-09-11 17:28:01 +0900

    OpenInCodeSandboxにCodeSandboxを表示させない機能を追加

--- src/components/OpenInCodeSandbox/index.jsx
+++ src/components/OpenInCodeSandbox/index.jsx
@@ -7,8 +7,9 @@ import styles from "./styles.module.css";
 /**
  * @param {Object} props
  * @param {boolean} props.path
+ * @param {boolean} props.noCodeSandbox
  */
-export default function OpenInCodeSandbox({ path }) {
+export default function OpenInCodeSandbox({ path, noCodeSandbox }) {
   return (
     <div className={styles.root}>
       <a
@@ -20,15 +21,17 @@ export default function OpenInCodeSandbox({ path }) {
         <SiGithub className={styles.icon} />
         GitHub で表示
       </a>
-      <a
-        className={clsx("button button--primary", styles.button)}
-        target="_blank"
-        rel="noopener"
-        href={`https://githubbox.com/ut-code/utcode-learn/tree/master${path}`}
-      >
-        このプログラムを実行する
-        <BiLinkExternal className={styles.icon} />
-      </a>
+      {!noCodeSandbox && (
+        <a
+          className={clsx("button button--primary", styles.button)}
+          target="_blank"
+          rel="noopener"
+          href={`https://githubbox.com/ut-code/utcode-learn/tree/master${path}`}
+        >
+          このプログラムを実行する
+          <BiLinkExternal className={styles.icon} />
+        </a>
+      )}
     </div>
   );
 }
