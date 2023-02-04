d0ad046865b1547eed355ad78628a95ce9b51140
chvmvd <chvmvd@users.noreply.github.com>
2023-02-04 23:13:38 +0900

    Create Answer component

---
+++ src/components/Answer/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Details from "@theme/Details";
+
+/**
+ * component that hide an answer
+ */
+export default function Answer({children}){
+    return <Details summary={<summary>解答</summary>}>{children}</Details>;
+}
