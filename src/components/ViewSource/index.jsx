ee891b42ab1f8d7ac37c88a48aff25ae6e74701f
chvmvd <chvmvd@users.noreply.github.com>
2023-02-10 16:30:25 +0900

    Revise ViewSource to be able to use relative path

--- src/components/ViewSource/index.jsx
+++ src/components/ViewSource/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import clsx from "clsx";
+import { useLocation } from "@docusaurus/router";
+import config from "@generated/docusaurus.config";
 import { BiLinkExternal } from "react-icons/bi";
 import { SiGithub } from "react-icons/si";
 import styles from "./styles.module.css";
@@ -10,13 +12,14 @@ import styles from "./styles.module.css";
  * @param {boolean} props.noCodeSandbox
  */
 export default function ViewSource({ path, noCodeSandbox }) {
+  const pathname = useLocation().pathname.slice(config.baseUrl.length - 1);
   return (
     <div className={styles.root}>
       <a
         className={clsx("button button--secondary", styles.button)}
         target="_blank"
         rel="noopener"
-        href={`https://github.com/ut-code/utcode-learn/tree/master${path}`}
+        href={`https://github.com/ut-code/utcode-learn/tree/master${pathname}${path}`}
       >
         <SiGithub className={styles.icon} />
         GitHub で表示
