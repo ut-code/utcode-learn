fe9b42e492b2c3d50b32d46e4224d0ceda57e8c8
chvmvd <chvmvd@users.noreply.github.com>
2023-02-11 16:42:31 +0900

    Improve ViewSource component

--- src/components/ViewSource/index.jsx
+++ src/components/ViewSource/index.jsx
@@ -12,7 +12,7 @@ import styles from "./styles.module.css";
 export default function ViewSource({ url, path, noCodeSandbox }) {
   const fullPathSplit = url.split("/");
   const pathFromDocs = fullPathSplit.slice(fullPathSplit.lastIndexOf("docs"));
-  const dirPath = pathFromDocs.slice(0, 3);
+  const dirPath = pathFromDocs.slice(0, pathFromDocs.length - 1);
   const relativePath = `${dirPath.join("/")}/${path}`;
   return (
     <div className={styles.root}>
@@ -30,7 +30,7 @@ export default function ViewSource({ url, path, noCodeSandbox }) {
           className={clsx("button button--primary", styles.button)}
           target="_blank"
           rel="noopener"
-          href={`https://githubbox.com/ut-code/utcode-learn/tree/master${path}`}
+          href={`https://githubbox.com/ut-code/utcode-learn/tree/master/${relativePath}`}
         >
           このプログラムを実行する
           <BiLinkExternal className={styles.icon} />
