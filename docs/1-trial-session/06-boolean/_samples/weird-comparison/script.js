d5fb49c7f006544c9f4e17f97cbcfb215b6f957c
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-19 18:10:23 +0900

    Ignore weird-comparison file from Prettier

--- docs/1-trial-session/06-boolean/_samples/weird-comparison/script.js
+++ docs/1-trial-session/06-boolean/_samples/weird-comparison/script.js
@@ -2,4 +2,5 @@ let takaoHeight = 599;
 let everestHeight = 8849;
 let fujiHeight = 3776;
 document.write(takaoHeight < everestHeight < fujiHeight);
-document.write((takaoHeight = everestHeight));
+// prettier-ignore
+document.write(takaoHeight = everestHeight);
