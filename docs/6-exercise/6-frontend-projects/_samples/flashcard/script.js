1660cb54b78ffb0ef862168e9779fac8abb10b56
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-03-19 05:29:21 +0900

    lec6をdocsに追加

---
+++ docs/6-exercise/6-frontend-projects/_samples/flashcard/script.js
@@ -0,0 +1,21 @@
+const questions = ["apple", "black", "cloud"];
+const answers = ["りんご", "黒", "雲"];
+
+let question = document.getElementById("question");
+
+let answer = document.getElementById("answer");
+let index = 0;
+
+question.textContent = questions[0];
+
+document.getElementById("answer-button").onclick = () => {
+  answer.textContent = answers[index];
+};
+document.getElementById("next-button").onclick = () => {
+  index += 1;
+  if (index === questions.length) {
+    index = 0;
+  }
+  question.textContent = questions[index];
+  answer.textContent = "";
+};
