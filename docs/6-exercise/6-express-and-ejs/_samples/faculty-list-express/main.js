01d47f69f6b0bc0ed80c21505bcd3e2bfad8d66d
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2023-04-10 15:20:25 +0900

    解答例修正

--- docs/6-exercise/6-express-and-ejs/_samples/faculty-list-express/main.js
+++ docs/6-exercise/6-express-and-ejs/_samples/faculty-list-express/main.js
@@ -4,19 +4,22 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 
-app.use((request, response) => {
-  if (request.url === "/") {
-    const template = fs.readFileSync("facultyList.ejs", "utf-8");
-    const html = ejs.render(template, {
-      faculties,
-      facultyData,
-    });
-    response.send(html);
-  } else if (faculties.includes(request.url.slice(1))) {
+app.get("/", (request, response) => {
+  const template = fs.readFileSync("facultyList.ejs", "utf-8");
+  const html = ejs.render(template, {
+    faculties,
+    facultyData,
+  });
+  response.send(html);
+});
+
+app.get("/:faculty", (request, response) => {
+  const faculty = request.params.faculty;
+  if (faculties.includes(faculty)) {
     const template = fs.readFileSync("departmentList.ejs", "utf-8");
     const html = ejs.render(template, {
       facultyData,
-      faculty: request.url.slice(1),
+      faculty,
     });
     response.send(html);
   }
