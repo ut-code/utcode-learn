5d0d40c3a0e7fa4366f596429e9ab5527d335df4
frgd <137767097+Fridge0@users.noreply.github.com>
2023-09-26 21:48:36 +0900

    Fixed docusaurus reference link of mudularization

--- src/components/Term/definitions.js
+++ src/components/Term/definitions.js
@@ -259,7 +259,7 @@ export default {
     javascriptModularization: {
       name: "モジュール化",
       definition: "ひとまとまりの複雑なものや操作を一つにまとめること。",
-      referencePage: "/docs/1-trial-session/09-functions/index.md/#モジュール化",
+      referencePage: "/docs/trial-session/functions/index.md/#モジュール化",
     },
     javascriptObject: {
       name: "オブジェクト (JavaScript)",
