a59727d55996b95aba9eec687d2f8f69cf58220e
Fridge0 <vrcraft0@gmail.com>
2023-09-26 22:48:02 +0900

    fixed example codes cuz they weren't even working

--- docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
+++ docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
@@ -1,21 +1,23 @@
 //第一引数の配列の第二引数、第三引数のインデックスの要素を入れ替える関数
 function swapIndex(array, indexA, indexB) {
     const arrayToChange = array.slice(); //配列の各値をコピー
-    const temp = array[indexA];
-    array[indexA] = array[indexB];
-    array[indexB] = temp;
+    const temp = arrayToChange[indexA];
+    arrayToChange[indexA] = arrayToChange[indexB];
+    arrayToChange[indexB] = temp;
+    return arrayToChange;
   }
   
   //本関数
   function bubbleSort(array) {
-    const willResult = array.slice(); // 配列の各値をコピー
+    let willResult = array.slice(); // 配列の各値をコピー
     for (let i = array.length - 1; i > 0; i--) {
       for (let j = 0; j < i; j++) {
         if(willResult[j]>willResult[j+1]) willResult = swapIndex(willResult, j, j + 1);
       }
     }
+    return willResult;
   }
   
-  unsortedArray = [9, 3, 1, 5, 8, 2, 4, 7, 6, 10];
-  document.write(`Sorted array: [${bubbleSort(unsortedArray).join(", ")}]`);
+  let unsortedArray = [9, 3, 1, 5, 8, 2, 4, 7, 6, 10];
+  document.write(`Sorted array: [${bubbleSort(unsortedArray).join(", ")}] <br>`);
   document.write(`Input Array(should not be sorted): [${unsortedArray.join(", ")}]`);
