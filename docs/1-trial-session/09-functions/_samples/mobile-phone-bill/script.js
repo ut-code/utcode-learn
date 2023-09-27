585405552b0b3f59566b773ec368808060868ce8
Fridge0 <vrcraft0@gmail.com>
2023-09-27 21:51:23 +0900

    _samplesの中身の変数命名を変更

--- docs/1-trial-session/09-functions/_samples/mobile-phone-bill/script.js
+++ docs/1-trial-session/09-functions/_samples/mobile-phone-bill/script.js
@@ -1,9 +1,8 @@
-function calculateCost(monthlyBandwidth) {
-  if (monthlyBandwidth < 5.0) {
-    return monthlyBandwidth * 600;
-  } else {
-    return 3000;
+function calculateCost(monthlyDataUsage) {
+  if (monthlyDataUsage< 5.0) {
+    return monthlyDataUsage * 600;
   }
+  return 3000;
 }
 
-document.write(calculateCost(3.5));
+document.write("通信量が3.5GBの場合、通信料金は" + calculateCost(3.5) + "円です。");
