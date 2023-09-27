debe5229daa6217f8dc02db58e3d365dc97fe15d
Fridge0 <vrcraft0@gmail.com>
2023-09-27 10:02:14 +0900

    prettier

--- docs/6-exercise/1-basis-of-web/_samples/bubble-sort/normal/script.js
+++ docs/6-exercise/1-basis-of-web/_samples/bubble-sort/normal/script.js
@@ -9,7 +9,7 @@ function swapIndex(array, indexA, indexB) {
 function bubbleSort(array) {
   for (let i = array.length - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
-      if(array[j]>array[j+1]) swapIndex(array, j, j + 1);
+      if (array[j] > array[j + 1]) swapIndex(array, j, j + 1);
     }
   }
 }
--- docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
+++ docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
@@ -1,23 +1,26 @@
 //第一引数の配列の第二引数、第三引数のインデックスの要素を入れ替える関数
 function swapIndex(array, indexA, indexB) {
-    const arrayToChange = array.slice(); //配列の各値をコピー
-    const temp = arrayToChange[indexA];
-    arrayToChange[indexA] = arrayToChange[indexB];
-    arrayToChange[indexB] = temp;
-    return arrayToChange;
-  }
-  
-  //本関数
-  function bubbleSort(array) {
-    let willResult = array.slice(); // 配列の各値をコピー
-    for (let i = array.length - 1; i > 0; i--) {
-      for (let j = 0; j < i; j++) {
-        if(willResult[j]>willResult[j+1]) willResult = swapIndex(willResult, j, j + 1);
-      }
+  const arrayToChange = array.slice(); //配列の各値をコピー
+  const temp = arrayToChange[indexA];
+  arrayToChange[indexA] = arrayToChange[indexB];
+  arrayToChange[indexB] = temp;
+  return arrayToChange;
+}
+
+//本関数
+function bubbleSort(array) {
+  let willResult = array.slice(); // 配列の各値をコピー
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (willResult[j] > willResult[j + 1])
+        willResult = swapIndex(willResult, j, j + 1);
     }
-    return willResult;
   }
-  
-  let unsortedArray = [9, 3, 1, 5, 8, 2, 4, 7, 6, 10];
-  document.write(`Sorted array: [${bubbleSort(unsortedArray).join(", ")}] <br>`);
-  document.write(`Input Array(should not be sorted): [${unsortedArray.join(", ")}]`);
+  return willResult;
+}
+
+let unsortedArray = [9, 3, 1, 5, 8, 2, 4, 7, 6, 10];
+document.write(`Sorted array: [${bubbleSort(unsortedArray).join(", ")}] <br>`);
+document.write(
+  `Input Array(should not be sorted): [${unsortedArray.join(", ")}]`,
+);
