390c008c80cd7e19376069dbe371e72a92d9ae9f
Fridge0 <vrcraft0@gmail.com>
2023-09-27 11:36:52 +0900

    変数名変更

--- docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
+++ docs/6-exercise/1-basis-of-web/_samples/bubble-sort/pure/script.js
@@ -1,22 +1,21 @@
 //第一引数の配列の第二引数、第三引数のインデックスの要素を入れ替える関数
-function swapIndex(array, indexA, indexB) {
-  const arrayToChange = array.slice(); //配列の各値をコピー
-  const temp = arrayToChange[indexA];
-  arrayToChange[indexA] = arrayToChange[indexB];
-  arrayToChange[indexB] = temp;
-  return arrayToChange;
+function swapIndex(inputArray, indexA, indexB) {
+  const newArray = inputArray.slice(); //配列の各値をコピー
+  const temp = newArray[indexA];
+  newArray[indexA] = newArray[indexB];
+  newArray[indexB] = temp;
+  return newArray;
 }
 
 //本関数
-function bubbleSort(array) {
-  let willResult = array.slice(); // 配列の各値をコピー
+function bubbleSort(inputArray) {
+  let array = inputArray.slice(); // 配列の値をコピー
   for (let i = array.length - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
-      if (willResult[j] > willResult[j + 1])
-        willResult = swapIndex(willResult, j, j + 1);
+      if (array[j] > array[j + 1]) swapIndex(array, j, j + 1);
     }
   }
-  return willResult;
+  return array;
 }
 
 let unsortedArray = [9, 3, 1, 5, 8, 2, 4, 7, 6, 10];
