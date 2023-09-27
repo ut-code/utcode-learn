bac91e64528f832bd0ea54f67daa732a715f98e0
Fridge0 <vrcraft0@gmail.com>
2023-09-27 23:12:56 +0900

    prettierと、解答例への照合

--- docs/1-trial-session/09-functions/_samples/mobile-phone-bill/script.js
+++ docs/1-trial-session/09-functions/_samples/mobile-phone-bill/script.js
@@ -1,8 +1,8 @@
 function calculateCost(monthlyDataUsage) {
-  if (monthlyDataUsage< 5.0) {
+  if (monthlyDataUsage < 5.0) {
     return monthlyDataUsage * 600;
   }
   return 3000;
 }
 
-document.write("通信量が3.5GBの場合、通信料金は" + calculateCost(3.5) + "円です。");
+document.write(calculateCost(3.5));
