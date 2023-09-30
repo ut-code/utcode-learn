5c961806660daa333a9ff680201596eecebdafdb
Fridge0 <vrcraft0@gmail.com>
2023-09-30 22:43:02 +0900

    temp

---
+++ src/components/Link/definitions.js
@@ -0,0 +1,174 @@
+/* list of available links
+
+  current session 1:
+    getStarted, html, jsStarter, jsValueAndExpression,
+    jsVariable, jsBoolean, jsIfStatement, jsFunctions,
+    jsLoop, jsArray, jsObject, cssStarter, jsDOM,
+    jsEvent, project1, deployOnNetlify,
+
+  current session 2:
+
+  current session 3:
+    wsl, nodejs, npm, httpServer, jsTemplateEngine,
+    httpForm, httpRequest, database, 
+
+*/
+export default {
+  pageLinks: {
+    getStarted: {
+      ref: "/docs/trial-session/get-started/",
+      title: "はじめてのWeb開発",
+    },
+    html: {
+      ref: "/docs/trial-session/html/",
+      title: "HTML",
+    },
+    jsStarter: {
+      ref: "/docs/trial-session/javascript/",
+      title: "JavaScriptことはじめ",
+    },
+    jsValueAndExpression: {
+      ref: "/docs/trial-session/expressions/",
+      title: "値と式と演算子",
+    },
+    jsVariable: {
+      ref: "/docs/trial-session/variables/",
+      title: "変数",
+    },
+    jsBoolean: {
+      ref: "/docs/trial-session/boolean/",
+      title: "論理値と論理演算子",
+    },
+    jsIfStatement: {
+      ref: "/docs/trial-session/if-statement/",
+      title: "条件分岐",
+    },
+    jsFunctions: {
+      ref: "/docs/trial-session/functions/",
+      title: "関数",
+    },
+    jsLoop: {
+      ref: "/docs/browser-apps/loop/",
+      title: "繰り返し",
+    },
+    jsArray: {
+      ref: "/docs/browser-apps/array/",
+      title: "配列",
+    },
+    jsObject: {
+      ref: "/docs/trial-session/object/",
+      title: "オブジェクト",
+    },
+    cssStarter: {
+      ref: "/docs/trial-session/css/",
+      title: "ウェブサイトの見た目を整える",
+    },
+    jsDOM: {
+      ref: "/docs/trial-session/dom/",
+      title: "DOM",
+    },
+    jsEvent: {
+      ref: "/docs/trial-session/events/",
+      title: "イベント",
+    },
+    project1: {
+      ref: "/docs/trial-session/project/",
+      title: "プロジェクト 1 / おみくじ",
+    },
+    deployOnNetlify: {
+      ref: "/docs/trial-session/deploy/",
+      title: "作ったウェブアプリを公開する",
+    },
+    jsInspector: {
+      ref: "/docs/browser-apps/inspector/",
+      title: "ブラウザの開発者ツール",
+    },
+    jsConst: {
+      ref: "/docs/browser-apps/constant/",
+      title: "定数とオブジェクトの参照",
+    },
+    jsReference: {
+      ref: "/docs/browser-apps/constant/#参照",
+    },
+    jsClass: {
+      ref: "/docs/browser-apps/class/",
+      title: "クラス",
+    },
+    jsAnonymousFunction: {
+      ref: "/docs/browser-apps/anonymous-function/",
+      title: "無名関数",
+    },
+    cssAdvanced: {
+      ref: "/docs/advanced/css/",
+      title: "高度な CSS",
+    },
+    cssAlignment: {
+      ref: "/docs/browser-apps/css-arrangement/",
+      title: "CSSによる配置",
+    },
+    project2: {
+      ref: "/docs/browser-apps/project/",
+      title: "プロジェクト 2 / カレンダー?"
+    },
+    gitAndGithub: {
+      ref: "/docs/browser-apps/git-github/",
+      title: "Git と GitHub",
+    },
+    wsl: {
+      ref: "/docs/web-servers/wsl-setup/",
+      title: "WSL のセットアップ",
+    },
+    nodejs: {
+      ref: "/docs/web-servers/node-js/",
+      title: "はじめての Node.js",
+    },
+    npm: {
+      ref: "/docs/web-servers/module/",
+      title: "モジュールと npm",
+    },
+    httpServer: {
+      ref: "/docs/web-servers/http-server/",
+      title: "HTTP サーバー",
+    },
+    jsTemplateEngine: {
+      ref: "/docs/web-servers/template-engine/",
+      title: "Express とテンプレートエンジン",
+    },
+    httpForm: {
+      ref: "/docs/web-servers/form/",
+      title: "フォーム",
+    },
+    httpRequest: {
+      ref: "/docs/web-servers/get-post/",
+      title: "GET リクエストと POST リクエスト",
+    },
+    database: {
+      ref: "/docs/web-servers/database/",
+      title: "データベース",
+    },
+    cookie: {
+      ref: "/docs/web-servers/cookie/",
+      title: "Cookie と認証",
+    },
+    deployOnRender: {
+      ref: "/docs/web-servers/deploy-on-render/",
+      title: "Render へのデプロイ",
+    },
+    fetchAPI: {
+      ref: "/docs/advanced/fetch-api/",
+      title: "Fetch API",
+    },
+    bundler: {
+      ref: "/docs/advanced/bundler/",
+      title: "トランスパイラとモジュールバンドラ",
+    },
+    typescript: {
+      ref: "/docs/advanced/typescript/",
+      title: "TypeScript",
+    },
+    react: {
+      ref: "/docs/advanced/react/",
+      title: "React",
+    },
+  },
+};
