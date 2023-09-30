b85269975b9d0d78faf9055702d1e5a2cbe3d88b
Fridge0 <vrcraft0@gmail.com>
2023-10-01 01:06:21 +0900

    #アンカーを除外

--- src/components/Term/index.jsx
+++ src/components/Term/index.jsx
@@ -27,9 +27,11 @@ export default function Term({ type, strong = false, children }) {
         // referencePageがundefinedならばリンクを表示しない (refPageTitleがundefでもデバッグの容易性のために表示する、直せ)
       if(term.referencePage === undefined) return false;
 
+        // referencePageの#アンカーを除外
+      const referenceLink = term.referencePage.split("#")[0];
         // 現在のページで用語が初出であればリンクを表示する必要がない
-      if(location.pathname === term.referencePage) return false;
-      if(location.pathname + "/" === term.referencePage) return false;
+      if(location.pathname === referenceLink) return false;
+      if(location.pathname + "/" === referenceLink) return false;
       return true;
     }
 
