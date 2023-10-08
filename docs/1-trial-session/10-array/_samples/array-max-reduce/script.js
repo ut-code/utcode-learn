bedf6d5b6cbb662da658796c54879843141a250c
Fridge0 <vrcraft0@gmail.com>
2023-10-08 22:16:59 +0900

    Little Fix (解答例の改行の追加)

--- docs/1-trial-session/10-array/_samples/array-max-reduce/script.js
+++ docs/1-trial-session/10-array/_samples/array-max-reduce/script.js
@@ -6,6 +6,7 @@ function arrayMax(array) {
   if (array == []) return; //空配列をエスケープ
   return array.reduce(max, array[0]);
 }
+
 const array1 = [5986, 7202, 9347, 3593, 8166, 662, 2235, 9323, 2240, 943];
 const array2 = [-878, -40, -324, -410, -592, -610, -880, -65, -423, -32];
 
