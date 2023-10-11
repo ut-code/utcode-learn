e4156a7bb7e2d082e51f3c50e1769e8ab3fcb2fb
Fridge0 <vrcraft0@gmail.com>
2023-10-11 14:26:44 +0900

    Changed the name of variable. from `integer` to `integralNumber`

--- docs/1-trial-session/08-loop/_samples/is-prime-using-and/script.js
+++ docs/1-trial-session/08-loop/_samples/is-prime-using-and/script.js
@@ -1,17 +1,17 @@
-let integer = 89; //任意の整数
+let integralNumber = 89; //任意の整数
 
 let isPrime = true;
-if (integer <= 1) {
+if (integralNumber <= 1) {
   isPrime = false;
 }
 
-for (let i = 2; i < integer; i += 1) {
-  // integerが1以下のときは素数でない
-  isPrime = isPrime && integer % i != 0;
+for (let i = 2; i < integralNumber; i += 1) {
+  // integralNumberが1以下のときは素数でない
+  isPrime = isPrime && integralNumber % i != 0;
 }
 
 if (isPrime) {
-  document.write(`${integer} は素数です。`);
+  document.write(`${integralNumber} は素数です。`);
 } else {
-  document.write(`${integer} は素数ではありません。`);
+  document.write(`${integralNumber} は素数ではありません。`);
 }
