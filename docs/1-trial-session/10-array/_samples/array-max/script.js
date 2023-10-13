cbc407aeb99694e9a547d64e3ea9b03380d50c3d
Fridge0 <vrcraft0@gmail.com>
2023-10-14 00:56:53 +0900

    match name in viewsource

--- docs/1-trial-session/10-array/_samples/array-max/script.js
+++ docs/1-trial-session/10-array/_samples/array-max/script.js
@@ -1,4 +1,4 @@
-function arrayMax(array) {
+function findMax(array) {
   if (array.length == 0) return; //空配列の例外処理
   let maxValue = array[0];
   for (let i = 0; i < array.length; i += 1) {
@@ -12,8 +12,8 @@ const array2 = [-8, -7, -3, -1, -5];
 const array3 = [5986, 7202, 9347, 3593, 8166, 662, 2235, 9323, 2240, 943];
 const array4 = [-878, -40, -324, -410, -592, -610, -880, -65, -423, -32];
 
-document.write(`<p>配列 [${array1}] の最大値は${arrayMax(array1)} です。</p>`);
-document.write(`<p>配列 [${array2}] の最大値は${arrayMax(array2)} です。</p/>`);
-document.write(`<p>配列 [${array3}] の最大値は${arrayMax(array3)} です。</p>`);
-document.write(`<p>配列 [${array4}] の最大値は${arrayMax(array4)} です。</p>`);
-document.write(`<p>空の配列の最大値は ${arrayMax([])} です。</p/>`);
+document.write(`<p>配列 [${array1}] の最大値は${findMax(array1)} です。</p>`);
+document.write(`<p>配列 [${array2}] の最大値は${findMax(array2)} です。</p/>`);
+document.write(`<p>配列 [${array3}] の最大値は${findMax(array3)} です。</p>`);
+document.write(`<p>配列 [${array4}] の最大値は${findMax(array4)} です。</p>`);
+document.write(`<p>空の配列の最大値は ${findMax([])} です。</p/>`);
