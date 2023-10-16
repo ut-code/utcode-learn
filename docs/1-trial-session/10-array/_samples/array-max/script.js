724d87e3a24afda4aa6752e75d82971480178e61
Fridge0 <vrcraft0@gmail.com>
2023-10-16 22:33:22 +0900

    pタグが壊れてたので修正

--- docs/1-trial-session/10-array/_samples/array-max/script.js
+++ docs/1-trial-session/10-array/_samples/array-max/script.js
@@ -15,7 +15,7 @@ const array3 = [5986, 7202, 9347, 3593, 8166, 662, 2235, 9323, 2240, 943];
 const array4 = [-878, -40, -324, -410, -592, -610, -880, -65, -423, -32];
 
 document.write(`<p>配列 [${array1}] の最大値は${findMax(array1)} です。</p>`);
-document.write(`<p>配列 [${array2}] の最大値は${findMax(array2)} です。</p/>`);
+document.write(`<p>配列 [${array2}] の最大値は${findMax(array2)} です。</p>`);
 document.write(`<p>配列 [${array3}] の最大値は${findMax(array3)} です。</p>`);
 document.write(`<p>配列 [${array4}] の最大値は${findMax(array4)} です。</p>`);
 document.write(`<p>空の配列の最大値は ${findMax([])} です。</p/>`);
