a14ba5e4d2ff1fc91cb9073bc0f891c32bce9f78
Fridge0 <vrcraft0@gmail.com>
2023-10-19 00:36:30 +0900

    made it look better

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -8,7 +8,7 @@ export default function Answer({ type = undefined, children }) {
   return (
     <Details
       summary={
-        type ? <summary>{type} の解答例</summary> : <summary>解答例</summary>
+        type ? <summary>解答例: {type}</summary> : <summary>解答例</summary>
       }
     >
       {children}
