08d8b47ff8d3533b5da7c095ac1902e8e5d7d33f
Fridge0 <vrcraft0@gmail.com>
2023-10-19 00:25:41 +0900

    解答例にタイトル付与可能

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -4,6 +4,14 @@ import Details from "@theme/Details";
 /**
  * component that hide an answer
  */
-export default function Answer({ children }) {
-  return <Details summary={<summary>解答例</summary>}>{children}</Details>;
+export default function Answer({ type = undefined, children }) {
+  return (
+    <Details
+      summary={
+        type ? <summary>{type} の解答例</summary> : <summary>解答例</summary>
+      }
+    >
+      {children}
+    </Details>
+  );
 }
