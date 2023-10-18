60b00ab1c95684e227514f1b485cdf85afee2578
Fridge0 <vrcraft0@gmail.com>
2023-10-19 00:19:34 +0900

    "解答" -> "解答例"

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -5,5 +5,5 @@ import Details from "@theme/Details";
  * component that hide an answer
  */
 export default function Answer({ children }) {
-  return <Details summary={<summary>解答</summary>}>{children}</Details>;
+  return <Details summary={<summary>解答例</summary>}>{children}</Details>;
 }
