1589d3f7c4137e0de1940fad8c9a8b736b0c6519
Fridge0 <vrcraft0@gmail.com>
2023-11-06 15:45:36 +0900

    解答のファイルの追加

---
+++ docs/2-browser-apps/02-reference/_samples/answer-spread/script.js
@@ -0,0 +1,10 @@
+function incrementAge(person) {
+  const newPerson = { ...person };
+  newPerson.age += 1;
+  return newPerson;
+}
+
+const tanaka = { name: "田中", age: 18 };
+const nextYearTanaka = incrementAge(tanaka);
+document.write(nextYearTanaka.age); // 19
+document.write(tanaka.age); // 18
