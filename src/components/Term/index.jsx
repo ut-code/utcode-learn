817732c1b5f19d752dd5ceeb9fe7a4ba24777f3d
Fridge0 <vrcraft0@gmail.com>
2023-11-08 12:10:16 +0900

    onlyText導入

--- src/components/Term/index.jsx
+++ src/components/Term/index.jsx
@@ -4,6 +4,7 @@ import Tippy from "@tippyjs/react";
 import { MdArrowForward } from "react-icons/md";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/material.css";
+import { onlyText } from "react-children-utilities";
 import styles from "./styles.module.css";
 import definitions from "./definitions";
 import shortDefinitions from "./short-definitions.js";
@@ -15,14 +16,10 @@ import typeMap from "./type-map.js";
  * @param {React.ReactNode} props.children
  */
 export default function Term({ type = null, children }) {
-  function unwrapNode(node) {
-    if (typeof node === "string") return node;
-    else return unwrapNode(node.props.children);
-  }
-  if (type === null) type = typeMap.get(unwrapNode(children));
+  if (type === null) type = typeMap.get(onlyText(children));
   if (!type)
     throw new Error(
-      `Problem: Term ${children.textContent} is not defined in type-map.js .
+      `Problem: Term ${onlyText(children)} is not defined in type-map.js .
       Solution: explicitly specify term type, or add type definition to type-map.js`,
     );
 
