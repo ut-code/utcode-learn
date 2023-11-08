44fa12e838139be38715c44dc518ee46ec4d5cdc
Fridge0 <vrcraft0@gmail.com>
2023-11-08 18:44:16 +0900

    完成

--- src/components/Term/type-map.js
+++ src/components/Term/type-map.js
@@ -3,10 +3,11 @@
   # term の追加
 > 文字列そのままの Term しか match しないので、
   説明的な Term は対応していません。
-  例: "演算子の優先順位"、"Truthy と Falsy"
+  例: "演算子の優先順位"
 
 > 名前の同じ Term は、使用頻度の高そうな方を優先してください。
   例: "プロパティ" -> "jsProperty"
+      "メソッド" -> "jsMethod"
 
 > 英数字と日本語の間は半角を入れることになっているので、
   半角が入っているもののみを記述してください。
@@ -17,10 +18,11 @@
 > Mapとして保存されています。 Map#get を使ってください
   例: const type = typeMap.get(termAsJapaneseString); // ./index.jsx
 
-> 説明的な Term (Truthy と Falsy　など) や
+> 説明的な Term (演算子の優先順位 など) や
   名前の同じ Term (CSS のプロパティ -> JS のプロパティとして解釈する)
   などは対応していないので、term を明示してください。
-  例: CSS の<Term type="cssProperty">プロパティ</Term> // /docs/
+  例: `*` は `+` より<Term type="operatorPriority">優先順位</Term>が高い
+      CSS の<Term type="cssProperty">プロパティ</Term> // /docs/
 */
 
 const typeMap = new Map([
@@ -43,6 +45,7 @@ const typeMap = new Map([
   ["文", "javascriptStatement"],
   ["値", "javascriptValue"],
   ["文字列", "javascriptString"],
+  ["数値", "javascriptNumber"],
   ["演算子", "javascriptOperator"],
   ["式", "javascriptExpression"],
   ["評価", "javascriptEvaluation"],
@@ -79,88 +82,36 @@ const typeMap = new Map([
   ["コンストラクタ", "javascriptConstructor"],
   ["メソッド", "javascriptMethod"],
   ["アロー関数", "arrowFunction"],
-  ["コールバック関数", "callBackFunction"],
+  ["コールバック関数", "callbackFunction"],
   ["モジュール", "javascriptModule"],
   ["ライブラリ", "library"],
+  ["サーバー", "serverClient"],
+  ["クライアント", "serverClient"],
   ["JSON", "json"],
+  ["HTTP リクエスト", "httpRequestResponse"],
+  ["リクエスト", "httpRequestResponse"],
+  ["レスポンス", "httpRequestResponse"],
   ["クエリ文字列", "queryString"],
+  ["クエリパラメータ", "queryString"],
+  ["リクエストヘッダ", "httpHeaderBody"],
+  ["リクエストボディ", "httpHeaderBody"],
+  ["レスポンスボディ", "httpHeaderBody"],
+  ["HTTP メソッド", "httpMethod"],
+  ["GET リクエスト", "httpMethod"],
+  ["POST リクエスト", "httpMethod"],
   ["npx コマンド", "npxCommand"],
   ["非同期処理", "asynchronousProcess"],
+  ["非同期関数", "asynchronousProcess"],
   ["API", "api"],
-  ["スレッド", "thread"],
+  ["スレッド", "thread"], // not found
   ["トランスパイル", "transpile"],
+  ["トランスパイラ", "transpile"],
   ["モジュールバンドラ", "moduleBundler"],
   ["JSX", "jsx"],
+  ["truthy", "javascriptTruthyFalsy"],
+  ["falsy", "javascriptTruthyFalsy"],
+  ["ミュータブル", "mutableImmutable"],
+  ["イミュータブル", "mutableImmutable"],
 ]);
 
 export default typeMap;
-
-/* for later use // no-more-meaningless-javascript
-const typeMap = new Map([
-  ["拡張子", "fileExtension"],
-  ["フォーク", "fork"],
-  ["Git", "git"],
-  ["GitHub", "github"],
-  ["Gitpod", "gitpod"],
-  ["リポジトリ", "repository"],
-  ["HTML", "html"],
-  ["JavaScript", "javascript"],
-  ["開始タグ", "startTag"],
-  ["タグ", "tag"],
-  ["属性", "attribute"],
-  ["HTML 要素", "element"],
-  ["要素", "element"],
-  ["終了タグ", "endTag"],
-  ["CSS", "css"],
-  ["style 属性", "styleAttribute"],
-  ["文", "statement"],
-  ["値", "value"],
-  ["文字列", "string"],
-  ["演算子", "operator"],
-  ["式", "expression"],
-  ["評価", "evaluation"],
-  ["変数", "variable"],
-  ["宣言", "declaration"],
-  ["代入", "assignment"],
-  ["キャメルケース", "camelCase"],
-  ["スネークケース", "snakeCase"],
-  ["パスカルケース", "pascalCase"],
-  ["真偽値", "bool"],
-  ["if 文", "ifStatement"],
-  ["制御構造", "controlFlow"],
-  ["関数", "function"],
-  ["引数", "parameter"],
-  ["渡す", "pass"],
-  ["戻り値", "returnValue"],
-  ["返す", "return"],
-  ["スコープ", "scope"],
-  ["モジュール化", "modularization"],
-  ["オブジェクト", "object"],
-  ["プリミティブ", "primitive"],
-  ["プロパティ", "property"],
-  ["DOM", "dom"],
-  ["イベント", "events"],
-  ["イベントハンドラ", "eventHandler"],
-  ["参照", "reference"],
-  ["副作用", "sideEffects"],
-  ["純粋関数", "pureFunction"],
-  ["参照透過性", "referenceTransparency"],
-  ["クラス", "class"],
-  ["インスタンス", "instance"],
-  ["コンストラクタ", "constructor"],
-  ["メソッド", "method"],
-  ["アロー関数", "arrowFunction"],
-  ["コールバック関数", "callBackFunction"],
-  ["モジュール", "module"],
-  ["ライブラリ", "library"],
-  ["JSON", "json"],
-  ["クエリ文字列", "queryString"],
-  ["npx コマンド", "npxCommand"],
-  ["非同期処理", "asyncProcess"],
-  ["API", "api"],
-  ["スレッド", "thread"],
-  ["トランスパイル", "transpile"],
-  ["モジュールバンドラ", "moduleBundler"],
-  ["JSX", "jsx"],
-]);
-*/
