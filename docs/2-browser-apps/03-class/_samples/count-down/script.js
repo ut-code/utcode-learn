f98ce5a54b8f2a045954a8015f1290c87d308752
Fridge0 <vrcraft0@gmail.com>
2023-11-09 14:32:45 +0900

    カウントダウンの演習問題修正

--- docs/2-browser-apps/03-class/_samples/count-down/script.js
+++ docs/2-browser-apps/03-class/_samples/count-down/script.js
@@ -1,12 +1,7 @@
-function count() {
-  const now = new Date();
-  const remainingHours = 24 - now.getHours();
-  const remainingsMinutes = 60 - now.getMinutes();
-  const remainingSeconds = 60 - now.getSeconds();
-  return `今日は残り${remainingHours}時間、${remainingsMinutes}分、${remainingSeconds}秒`;
-}
-const box = document.getElementById("box");
+const countdownBox = document.getElementById("countdown-box");
 
-setInterval(() => {
-  box.textContent = count();
-}, 1000);
+const now = new Date();
+const remainingHours = 24 - now.getHours();
+const remainingMinutes = 60 - now.getMinutes();
+const remainingSeconds = 60 - now.getSeconds();
+countdownBox.textContent = `今日は残り${remainingHours}時間 ${remainingMinutes}分 ${remainingSeconds}秒です。`;
