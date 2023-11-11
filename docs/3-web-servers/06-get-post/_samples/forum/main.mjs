0b563dcd58959a602460f73f2b457521ec6cb2fa
Fridge0 <vrcraft0@gmail.com>
2023-11-11 12:53:36 +0900

    forum

--- docs/3-web-servers/06-get-post/_samples/forum/main.mjs
+++ docs/3-web-servers/06-get-post/_samples/forum/main.mjs
@@ -7,29 +7,14 @@ const messages = [];
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
-  response.send(`
-    <!doctype html>
-    <html lang="ja">
-      <head>
-        <meta charset="UTF-8" />
-        <title>Document</title>
-      </head>
-      <body>
-        <ul>
-          ${messages.map((message) => `<li>${message}</li>`).join("")}
-        </ul>
-        <form action="/send" method="post">
-          <input name="message" />
-          <button>送信</button>
-        </form>
-      </body>
-    </html>
-  `);
+  const template = fs.readFileSync("index.html");
+  const html = template.replace("{messages}", messages.map((msg) => `<li>${msg}</li>`).join(""));
+  response.send(html);
 });
 
 app.post("/send", (request, response) => {
   messages.push(request.body.message);
-  response.send("送信しました");
+  response.send("<body>送信しました</body>");
 });
 
 app.listen(3000);
