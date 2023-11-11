d33313149cbfba22f6964004998bf55e7eba1dae
Fridge0 <vrcraft0@gmail.com>
2023-11-11 11:36:05 +0900

    class-carのweightを文字列->数値に

--- docs/2-browser-apps/03-class/_samples/class-car/script.js
+++ docs/2-browser-apps/03-class/_samples/class-car/script.js
@@ -1,9 +1,9 @@
 class Car {
-  weight = "1t";
+  weight_tons = 1;
   cost;
 }
 
 const prius = new Car();
 prius.cost = 2600000;
 
-document.write(`重さは${prius.weight}で、値段は${prius.cost}円です。`);
+document.write(`重さは${prius.weight_tons}tで、値段は${prius.cost}円です。`);
