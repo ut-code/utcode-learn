6b0c9c95188ff9132d48e1520bf213101d83c63d
Fridge0 <vrcraft0@gmail.com>
2023-11-16 23:56:23 +0900

    別解の追加・本解答例の修正

--- docs/2-browser-apps/02-reference/_samples/answer-spread copy/script.js
+++ docs/2-browser-apps/02-reference/_samples/answer-spread copy/script.js
@@ -1,10 +1,8 @@
 function incrementAge(person) {
-  const newPerson = { ...person };
-  newPerson.age += 1;
-  return newPerson;
+  return { ...person, age: person.age + 1 };
 }
 
-const tanaka = { name: "田中", age: 18, address: "東京" };
+const tanaka = { name: "田中", age: 18 };
 const nextYearTanaka = incrementAge(tanaka);
 document.write(nextYearTanaka.age); // 19
 document.write(nextYearTanaka.address); // 東京
