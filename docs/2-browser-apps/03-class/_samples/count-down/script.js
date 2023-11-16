a04b11d90df6f6b5e8414f72e8c4b0be7861080e
Fridge0 <vrcraft0@gmail.com>
2023-11-17 01:40:22 +0900

    fix date exercise

--- docs/2-browser-apps/03-class/_samples/count-down/script.js
+++ docs/2-browser-apps/03-class/_samples/count-down/script.js
@@ -1,7 +1,17 @@
 const countdownBox = document.getElementById("countdown-box");
 
 const now = new Date();
-const remainingHours = 23 - now.getHours();
-const remainingMinutes = 59 - now.getMinutes();
-const remainingSeconds = 60 - now.getSeconds();
+let remainingHours = 23 - now.getHours();
+let remainingMinutes = 59 - now.getMinutes();
+let remainingSeconds = 60 - now.getSeconds();
+// remainingSeconds が 60 の時は繰り上げる
+if (remainingSeconds === 60) {
+  remainingMinutes += 1;
+  remainingSeconds = 0;
+}
+// remainingMinutes が 60 の時は繰り上げる
+if (remainingMinutes === 60) {
+  remainingHours += 1;
+  remainingMinutes = 0;
+}
 countdownBox.textContent = `今日は残り${remainingHours}時間${remainingMinutes}分${remainingSeconds}秒です。`;
