f0fbf714879c52488cf13cbef364cc75d0fbc268
aster <137767097+aster-void@users.noreply.github.com>
2024-03-30 17:04:13 +0900

    change answer of is-prime

--- docs/1-trial-session/09-loop/_samples/is-prime/script.js
+++ docs/1-trial-session/09-loop/_samples/is-prime/script.js
@@ -1,19 +1,18 @@
-const n = 57; // 任意の整数
-
-let isPrime = true;
-if (n <= 1) {
-  // nが1以下のときは素数でない
-  isPrime = false;
-}
-
-for (let i = 2; i < n; i += 1) {
-  if (n % i === 0) {
-    isPrime = false;
+function checkPrime(n) {
+  if (n <= 1) {
+    // nが1以下のときは素数ではない
+    return false;
   }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      // nはiで割り切れる
+      return false;
+    }
+  }
+  // 2..n-1 までのすべてのiで割り切れなかった時のみ到達する
+  return true;
 }
 
-if (isPrime) {
-  document.write(`${n} は素数です`);
-} else {
-  document.write(`${n} は素数ではありません`);
-}
+document.write(checkPrime(7)); // true
+document.write(checkPrime(57)); // false
+document.write(checkPrime(-1)); // false
