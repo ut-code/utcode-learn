59b36359c6b04597cbdc01136e5b77b38eaddd0c
aster <137767097+aster-void@users.noreply.github.com>
2024-04-20 18:37:52 +0900

    fixed things

--- docs/2-browser-apps/02-reference/_samples/answer-spread copy/script.js
+++
@@ -1,8 +0,0 @@
-function incrementAge(person) {
-  return { ...person, age: person.age + 1 };
-}
-
-const tanaka = { name: "田中", age: 18 };
-const nextYearTanaka = incrementAge(tanaka);
-document.write(nextYearTanaka.age); // 19
-document.write(nextYearTanaka.address); // 東京
--- docs/2-browser-apps/02-reference/_samples/answer-spread/script.js
+++ docs/2-browser-apps/02-reference/_samples/answer-spread/script.js
@@ -1,10 +1,9 @@
 function incrementAge(person) {
-  const newPerson = { ...person };
-  newPerson.age += 1;
+  const newPerson = { ...person, age: person.age + 1 };
   return newPerson;
 }
 
-const tanaka = { name: "田中", age: 18, address: "東京" };
+const tanaka = { name: "田中", age: 18 };
 const nextYearTanaka = incrementAge(tanaka);
 document.write(nextYearTanaka.age); // 19
-document.write(nextYearTanaka.address); // 東京
+document.write(tanaka.age); // 18
