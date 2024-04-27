467b773c8d5ac7d0fdb64e0180b9d184dbec9f62
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-04-27 23:03:01 +0900

    fix: 上の部分の記述を修正

--- docs/1-trial-session/13-dom/_samples/add-element/script.js
+++ docs/1-trial-session/13-dom/_samples/add-element/script.js
@@ -1,7 +1,5 @@
-const packingList = document.getElementById("packing-list");
-
 const newItem = document.createElement("li");
-
 newItem.textContent = "おやつ";
 
+const packingList = document.getElementById("packing-list");
 packingList.appendChild(newItem);
