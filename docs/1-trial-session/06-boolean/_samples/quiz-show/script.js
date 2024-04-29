ee49f8762d16251fab3effe56612985a54e7022b
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-04-29 23:07:18 +0900

    説明文と変数名の改善

--- docs/1-trial-session/06-boolean/_samples/quiz-show/script.js
+++ docs/1-trial-session/06-boolean/_samples/quiz-show/script.js
@@ -1,4 +1,4 @@
 const myScore = 120;
 const opponentScore = 80;
-const iWon = myScore >= 100 && myScore - opponentScore >= 20;
-document.write(iWon); // true
+const amITheWinner = myScore >= 100 && myScore - opponentScore >= 20;
+document.write(amITheWinner); // true
