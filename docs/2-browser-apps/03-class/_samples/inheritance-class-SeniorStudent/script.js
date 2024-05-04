9d1e3919f66c007aff89c012368d8829bf9436f1
WATAHIKI Yuto / 綿引 悠人 <104971044+chvmvd@users.noreply.github.com>
2024-05-04 11:33:25 +0900

    CodeSandboxに表示される解答が更新されていなかったため、更新 (#745)

--- docs/2-browser-apps/03-class/_samples/inheritance-class-SeniorStudent/script.js
+++ docs/2-browser-apps/03-class/_samples/inheritance-class-SeniorStudent/script.js
@@ -2,6 +2,11 @@ class Student {
   name;
   age;
 
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
   introduceSelf() {
     document.write(`私の名前は${this.name}です。${this.age}歳です。`);
   }
@@ -10,14 +15,15 @@ class Student {
 class SeniorStudent extends Student {
   researchQuestion;
 
+  constructor(name, age, researchQuestion) {
+    super(name, age);
+    this.researchQuestion = researchQuestion;
+  }
+
   introduceSelf() {
     super.introduceSelf();
     document.write(`研究テーマは${this.researchQuestion}です。`);
   }
 }
-
-const tanaka = new SeniorStudent();
-tanaka.age = 22;
-tanaka.name = "田中";
-tanaka.researchQuestion = "量子力学";
+const tanaka = new SeniorStudent("田中", 22, "量子力学");
 tanaka.introduceSelf();
