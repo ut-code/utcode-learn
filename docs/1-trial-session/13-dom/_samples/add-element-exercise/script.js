5b75848fa3c6700b9adeed428f922ea42b228a3b
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-05-13 15:50:52 +0900

    「要素を追加する」の確認問題を例題の続きに変更

--- docs/1-trial-session/13-dom/_samples/add-element-exercise/script.js
+++ docs/1-trial-session/13-dom/_samples/add-element-exercise/script.js
@@ -4,3 +4,8 @@ const snackItem = document.createElement("li");
 snackItem.textContent = "おやつ";
 
 packingList.appendChild(snackItem);
+
+const capItem = document.createElement("li");
+capItem.textContent = "帽子";
+
+packingList.appendChild(capItem);
--- docs/1-trial-session/13-dom/_samples/hello-world-bold/script.js
+++
@@ -1,5 +0,0 @@
-const strongElement = document.createElement("strong");
-strongElement.textContent = "Hello World!";
-
-const greetingElement = document.getElementById("greeting");
-greetingElement.appendChild(strongElement);
