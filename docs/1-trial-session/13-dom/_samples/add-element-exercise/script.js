a1e7add9b04f20db9ce02513970c7640c0eb9b1c
Shogo NAKAMURA <104970808+naka-12@users.noreply.github.com>
2024-05-14 13:17:16 +0900

    DOM の章の確認問題を追加、内容のアップデート (#725)

--- docs/1-trial-session/13-dom/_samples/add-element-exercise/script.js
+++ docs/1-trial-session/13-dom/_samples/add-element-exercise/script.js
@@ -4,3 +4,8 @@ const snackElement = document.createElement("li");
 snackElement.textContent = "おやつ";
 
 packingList.appendChild(snackElement);
+
+const capElement = document.createElement("li");
+capElement.textContent = "帽子";
+
+packingList.appendChild(capElement);
