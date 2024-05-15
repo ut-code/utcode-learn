cc5aae3ddf6cdbda43a740df627712352cc331b8
WATAHIKI Yuto / 綿引 悠人 <104971044+chvmvd@users.noreply.github.com>
2024-05-15 09:22:20 +0900

    Carクラスを定義する問題での単位の表記を修正 (#769)

--- docs/2-browser-apps/03-class/_samples/car-class/script.js
+++ docs/2-browser-apps/03-class/_samples/car-class/script.js
@@ -6,4 +6,6 @@ class Car {
 const prius = new Car();
 prius.cost = 2600000;
 
-document.write(`重さは${prius.weightInTons}tで、値段は${prius.cost}円です。`);
+document.write(
+  `重さは${prius.weightInTons}トンで、値段は${prius.cost}円です。`,
+);
