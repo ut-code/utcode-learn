af883ea57d17da4622008d6c773dcdff5618d6dd
chelproc <chelproc@outlook.com>
2024-06-02 23:07:48 +0900

    Express の章の再構成 / ミーティング後レビュー修正

--- docs/3-web-servers/05-server/_samples/express-server/main.mjs
+++ docs/3-web-servers/05-server/_samples/express-server/main.mjs
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send('Hello World! <a href="./lang/ja">日本語</a>');
+  response.send(`Hello World! <a href="./lang/ja">日本語</a>`);
 });
 app.get("/lang/ja", (request, response) => {
   response.send("こんにちは、世界！");
--- docs/3-web-servers/05-server/_samples/html-external-resources/main.mjs
+++ docs/3-web-servers/05-server/_samples/html-external-resources/main.mjs
@@ -2,10 +2,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send('<script src="./script.js"></script>');
+  response.send(`<script src="./script.js"></script>`);
 });
 app.get("/script.js", (request, response) => {
-  response.send('document.write("Hello World");');
+  response.send(`document.write("Hello World");`);
 });
 
 app.listen(3000);
