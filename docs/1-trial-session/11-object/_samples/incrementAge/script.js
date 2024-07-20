e5620e25c46a3d6f94970c51ba8d60fb22c594d2
chvmvd <104971044+chvmvd@users.noreply.github.com>
2024-07-20 12:13:54 +0900

    和欧文間の半角スペースを削除

--- docs/1-trial-session/11-object/_samples/incrementAge/script.js
+++ docs/1-trial-session/11-object/_samples/incrementAge/script.js
@@ -7,6 +7,4 @@ const tanaka = { name: "田中", age: 18 };
 const sato = { name: "佐藤", age: 22 };
 const nextYearTanaka = incrementAge(tanaka);
 const nextYearSato = incrementAge(sato);
-document.write(
-  `田中は ${nextYearTanaka.age} 歳、佐藤は ${nextYearSato.age} 歳`,
-);
+document.write(`田中は${nextYearTanaka.age}歳、佐藤は${nextYearSato.age}歳`);
