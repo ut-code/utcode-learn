b36191ef46563d5a22e170299ac11ad240673366
WATAHIKI Yuto <chvmvd@gmail.com>
2024-09-13 23:35:26 +0800

    確認問題の例を変更

--- docs/1-trial-session/12-object/_samples/change-price-property/script.js
+++
@@ -1,9 +0,0 @@
-const car = {
-  make: "トヨタ",
-  model: "プリウス",
-  price: 3000000,
-};
-
-document.write(`車の値段は${car.price}円です。`); // 車の値段は2500000円です。
-car.price = 3000000;
-document.write(`車の値段は${car.price}円です。`); // 車の値段は3000000円です。
--- docs/1-trial-session/12-object/_samples/create-car-object/script.js
+++
@@ -1,5 +0,0 @@
-const car = {
-  make: "トヨタ",
-  model: "プリウス",
-  price: 2500000,
-};
