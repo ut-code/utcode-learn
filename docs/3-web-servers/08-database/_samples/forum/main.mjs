eda21d51a0e486f07c6375b0b1bd189a3e7d8520
chelproc <chelproc@outlook.com>
2025-05-04 11:53:53 +0900

    @prisma/client のインポート元を修正

--- docs/3-web-servers/08-database/_samples/forum/main.mjs
+++ docs/3-web-servers/08-database/_samples/forum/main.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/index.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
