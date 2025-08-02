bfc30e742ee2a03e36893e86e0776b56a59c4a1a
chelproc <chelproc@outlook.com>
2025-08-02 17:48:58 +0900

    Fetch API で JSON を扱うための記事、POST リクエストを扱うための記事を追加

---
+++ docs/3-web-servers/06-fetch-api/_samples/fetch-weather-json/public/script.js
@@ -0,0 +1,8 @@
+async function fetchWeather() {
+  const response = await fetch("/weather");
+  // レスポンスをJSONとして取得
+  const weather = await response.json();
+  document.getElementById("condition").textContent = weather.condition;
+  document.getElementById("temperature").textContent = weather.temperature;
+}
+fetchWeather();
--- docs/3-web-servers/06-fetch-api/_samples/fetch-weather/public/script.mjs
+++
@@ -1,8 +0,0 @@
-// http://localhost:3000/weather に HTTP リクエストを送信
-// fetch 関数は非同期処理を行うため、await を演算子を適用して完了を待機する
-const response = await fetch("/weather");
-
-// レスポンスをテキストとして取得
-const weather = await response.text();
-
-document.getElementById("weather").textContent = weather;
