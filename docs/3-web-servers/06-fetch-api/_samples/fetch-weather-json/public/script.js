e03549f4f449213a512ec9169f4ef9d3eca5b779
chelproc <chelproc@outlook.com>
2025-08-09 21:57:46 +0900

    レスポンスの取得に関する表現を簡略化

--- docs/3-web-servers/06-fetch-api/_samples/fetch-weather-json/public/script.js
+++ docs/3-web-servers/06-fetch-api/_samples/fetch-weather-json/public/script.js
@@ -1,6 +1,5 @@
 async function fetchWeather() {
   const response = await fetch("/weather");
-  // レスポンスをJSONとして取得
   const weather = await response.json();
   document.getElementById("condition").textContent = weather.condition;
   document.getElementById("temperature").textContent = weather.temperature;
--- docs/3-web-servers/06-fetch-api/_samples/fetch-weather/public/script.js
+++ docs/3-web-servers/06-fetch-api/_samples/fetch-weather/public/script.js
@@ -3,7 +3,6 @@ async function fetchWeather() {
   // http://localhost:3000/weather にHTTPリクエストを送信
   // fetch関数は非同期処理を行うため、await演算子を適用して完了を待機する
   const response = await fetch("/weather");
-  // レスポンスをテキストとして取得
   const weather = await response.text();
   document.getElementById("weather").textContent = weather;
 }
