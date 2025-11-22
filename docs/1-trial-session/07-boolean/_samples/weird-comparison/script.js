25a0a33b292ba3da8ef0b4e1eedb43345aaad3ea
Rei Tsukada <rayzou1120@icloud.com>
2025-11-22 18:22:02 +0900

    不要なファイルを削除

--- docs/1-trial-session/07-boolean/_samples/weird-comparison/script.js
+++
@@ -1,6 +0,0 @@
-let takaoHeight = 599;
-let everestHeight = 8849;
-let fujiHeight = 3776;
-document.write(takaoHeight < everestHeight < fujiHeight);
-// prettier-ignore
-document.write(takaoHeight = everestHeight);
