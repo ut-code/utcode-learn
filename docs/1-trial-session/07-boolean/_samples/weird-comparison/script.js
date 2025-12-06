84690843b55d457a84ed07fb3425dde7b28d1ad2
Rei Tsukada <rayzou1120@icloud.com>
2025-12-06 20:24:39 +0900

    weired comparisonはview soruceのままに

---
+++ docs/1-trial-session/07-boolean/_samples/weird-comparison/script.js
@@ -0,0 +1,6 @@
+let takaoHeight = 599;
+let everestHeight = 8849;
+let fujiHeight = 3776;
+document.write(takaoHeight < everestHeight < fujiHeight);
+// prettier-ignore
+document.write(takaoHeight = everestHeight);
