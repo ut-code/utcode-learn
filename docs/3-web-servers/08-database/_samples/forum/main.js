9bde3843270d1b23f1f599d1a29fec279b1bed7e
chelproc <chelproc@outlook.com>
2026-05-09 19:08:17 +0900

    サンプルを最新の Prisma で動作するよう修正

--- docs/3-web-servers/08-database/_samples/forum/main.js
+++ docs/3-web-servers/08-database/_samples/forum/main.js
@@ -1,18 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/index.js";
 
 const app = express();
-const client = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter: adapter });
+
 app.use(express.json());
 app.use(express.static("./public"));
 
 app.get("/posts", async (request, response) => {
-  const posts = await client.post.findMany();
+  const posts = await prisma.post.findMany();
   response.json(posts);
 });
 
 app.post("/posts", async (request, response) => {
-  await client.post.create({ data: { message: request.body.message } });
+  await prisma.post.create({ data: { message: request.body.message } });
   response.sendStatus(201); // Created（新しいメッセージを作成）
 });
 
