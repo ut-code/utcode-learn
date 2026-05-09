95edf7bff752828e05c12be65526f070d09bfd61
chelproc <chelproc@outlook.com>
2026-05-09 19:31:45 +0900

    prisma-client-js が deprecated のため prisma-client に移行

--- docs/3-web-servers/08-database/_samples/forum/main.js
+++ docs/3-web-servers/08-database/_samples/forum/main.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/index.js";
+import { PrismaClient } from "./generated/prisma/client.ts";
 
 const app = express();
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
